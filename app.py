6e9e2a40ef17f6dc797d3e1caf5ab6368db7724d
Jessica <122835244+jfrostty@users.noreply.github.com>
2023-11-29 21:56:05 -0800

    Update app.py
    added PCA for user input and normalized grade output

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
+# PCA values in pca_components.py
+from pca_components import pca_mean, pca_stdev, projection_matrix
+from pca_components import pca_grade_mean, pca_grade_stdev
 
 app = Flask(__name__)
 
@@ -15,12 +18,21 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
-    features = [np.array(int_features)]
-    prediction = model.predict(features)
 
-    output = round(prediction[0], 2)
+    # calculate user input values after PCA
+    features = np.array(int_features)
+    features_standardized = (features - pca_mean) / pca_stdev
+    features_pca = np.dot(features_standardized, projection_matrix.T[:,:19])
 
-    return render_template('index.html', prediction_text='G3: {}'.format(output))
+    prediction = model.predict(features_pca.reshape(1, -1))
+    output = prediction[0]
+
+    # revert grade back from standardized and calculate % grade
+    grade = (output * pca_grade_stdev) + pca_grade_mean
+    grade = grade / 20 * 100
+    grade = round(grade, 2)
+
+    return render_template('index.html', prediction_text='G3: {}%'.format(grade))
 
 if __name__ == "__main__":
     app.run()
